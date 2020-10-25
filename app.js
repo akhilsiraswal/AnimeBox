5dc399d2a11216643e4d6c0c9b6ec0bd24abf8e0
Akhil Siraswal <akhilsiraswal@gmail.com>
2020-10-25 23:53:02 +0530

    25-10-20

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
 const express = require("express");
 const Jikan = require("jikan-node");
 const app = express();
+const bodyParser = require("body-parser");
 
 const anime = new Jikan();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("Images"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
-
-function returnSynopsis(data) {
-  console.log(data);
-  data.forEach((dat) => {
-    return dat.synopsis;
-  });
-}
-
+app.set("view engine", "ejs");
 app.get("/anime", (req, res) => {
-  // anime.findAnime("11597", "episodes", 1).then((animee) => res.send(animee));
   let animeData;
+  const animeToSearch = req.query.search;
+  var title, image, synopsis, type;
   anime
-    .search("manga", "naruto")
+    .search("manga", animeToSearch)
     .then((data) => {
       animeData = data.results;
-      animeData = animeData.filter((an) => {
-        return an.synopsis;
+      res.render("animes", {
+        animeData: animeData,
       });
-      console.log(animeData);
-      res.json(animeData);
+      // animeData.map((dat) => {
+      //   title = dat.title;
+      //   image = dat.image_url;
+      //   synopsis = dat.synopsis;
+      //   type = dat.type;
+      // });
+      // console.log(animeData);
     })
     .catch((err) => console.log(err));
 });
-
-// app.get('/anime/11597/episodes',(req,res) =>{
-//   anime.
-// })
-
-app.use(express.static("Images"));
 
 app.listen(8000, (req, res) => {
   console.log("Server is running ");
